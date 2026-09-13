3be093f1c8dbbbd18e32c36c8c9fffc7929d67d1
evandrosmagela-tech <evandrosmagela@hotmail.com>
2026-09-13 08:48:38 -0300

    Update planejadororochiking.js

--- planejadororochiking.js
+++ planejadororochiking.js
@@ -25,7 +25,7 @@
   /* ============================================================
      LIBERAÇÃO POR NICK
   ============================================================ */
-  var NICKS_LIBERADOS = ['Orochi.2009', 'Juniro1717', 'Jordy Alba', 'Bleda', 'EliteTeam5', 'Mr-magg'];
+  var NICKS_LIBERADOS = ['- Orochi.2009', 'Juniro1717', 'Jordy Alba', 'Bleda', 'EliteTeam5', 'Mr-magg'];
 
   function nickAtual() {
     try {
